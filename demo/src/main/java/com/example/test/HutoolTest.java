510dd2838d71ed2eddc732b14ac261619731d696
sky李齐 <sky.li@ixiaoshuidi.com>
2019-06-27 08:55:59 +0800

    监听redis 队列

--- demo/src/main/java/com/example/test/HutoolTest.java
+++ demo/src/main/java/com/example/test/HutoolTest.java
@@ -1,6 +1,5 @@
 package com.example.test;
 
-import cn.hutool.core.io.file.FileReader;
 
 import java.math.BigDecimal;
 import java.util.*;
@@ -12,22 +11,20 @@ import java.util.*;
  **/
 public class HutoolTest {
     public static void main(String[] args) {
-
-        List<String> list=new ArrayList<>();
+        List<String> list = new ArrayList<>();
         list.add("a");
         list.add("b");
-        list.add(0,"c");
+        list.add(0, "c");
         System.out.println(list.toString());
-
-        StringBuffer sb =new StringBuffer("hahahha");
-        String s1=null;
-        String s2=null;
+        StringBuffer sb = new StringBuffer("hahahha");
+        String s1 = null;
+        String s2 = null;
         sb.append(s1).append(s2);
         System.out.println(sb.toString());
 
-        Map<String,String> map=new HashMap<>();
-        map.put(null,"ahh");
-        map.put("hee","aha");
+        Map<String, String> map = new HashMap<>();
+        map.put(null, "ahh");
+        map.put("hee", "aha");
         System.out.println(String.valueOf((int) ((Math.random() * 9 + 1) * 100000)));
         System.out.println("hah".equals(null));
     }
