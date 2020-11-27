6caeeae6ab2feb0ba540521d4524ab5fdf5aefc8
liqi <sky.li>
2020-11-27 10:33:01 +0800

    springboot重定向

--- demo/src/main/java/com/example/java8LocalDataTime/LocalDateTimeTest.java
+++ demo/src/main/java/com/example/java8LocalDataTime/LocalDateTimeTest.java
@@ -1,13 +1,13 @@
 package com.example.java8LocalDataTime;
 
-import lombok.Data;
-
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAdjuster;
 import java.time.temporal.TemporalAdjusters;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Sky
@@ -17,6 +17,28 @@ import java.time.temporal.TemporalAdjusters;
 public class LocalDateTimeTest {
 
     public static void main(String[] args) {
+//        method01();
+        method02();
+    }
+
+
+    private static void method02() {
+        System.out.println(Instant.now());
+        System.out.println(LocalDateTime.now());
+
+        Period fromUnits = Period.of(3, 10, 10);
+        assertEquals(10, fromUnits.getDays());
+        assertEquals(10, fromUnits.getMonths());
+        assertEquals(3,fromUnits.getYears());
+
+        Period fromDays = Period.ofDays(50);
+        Period fromMonths = Period.ofMonths(5);
+        Period fromYears = Period.ofYears(10);
+        Period fromWeeks = Period.ofWeeks(40);
+        assertEquals(280, fromWeeks.getDays());
+    }
+
+    private static void method01() {
         DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         LocalDateTime startTime = LocalDateTime.now();
         LocalDateTime endTime = startTime.plusDays(7);
