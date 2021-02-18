0562244495f2a1c18af34b4e9f68ae2f2c419f4c
liqi <sky.li>
2021-02-18 14:47:39 +0800

    eumnSet,enumMap

---
+++ demo/src/main/java/com/example/enumTest/Pizza.java
@@ -0,0 +1,83 @@
+package com.example.enumTest;
+
+import lombok.Data;
+
+import java.util.EnumSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Sky
+ * create  2021-02-18 14:26
+ * email sky.li@ixiaoshuidi.com
+ */
+@Data
+public class Pizza {
+
+    private static EnumSet<PizzaStatus> undeliveredPizzaStatuses =
+            EnumSet.of(PizzaStatus.ORDERED, PizzaStatus.READY);
+
+    private PizzaStatus status;
+
+    public enum PizzaStatus {
+        ORDERED(5) {
+            @Override
+            public boolean isOrdered() {
+                return true;
+            }
+        },
+        READY(2) {
+            @Override
+            public boolean isReady() {
+                return true;
+            }
+        },
+        DELIVERED(0) {
+            @Override
+            public boolean isDelivered() {
+                return true;
+            }
+        };
+
+        private int timeToDelivery;
+
+        public boolean isOrdered() {
+            return false;
+        }
+
+        public boolean isReady() {
+            return false;
+        }
+
+        public boolean isDelivered() {
+            return false;
+        }
+
+        public int getTimeToDelivery() {
+            return timeToDelivery;
+        }
+
+        PizzaStatus(int timeToDelivery) {
+            this.timeToDelivery = timeToDelivery;
+        }
+    }
+
+    public boolean isDeliverable() {
+        return this.status.isReady();
+    }
+
+    public void printTimeToDeliver() {
+        System.out.println("Time to delivery is " +
+                this.getStatus().getTimeToDelivery());
+    }
+
+    public static List<Pizza> getAllUndeliveredPizzas(List<Pizza> input) {
+        return input.stream().filter(
+                (s) -> undeliveredPizzaStatuses.contains(s.getStatus()))
+                .collect(Collectors.toList());
+    }
+
+
+    // Methods that set and get the status variable.
+}
+
